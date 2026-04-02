e7bc43e0edf834ae825dce8bd42fd311105d38c0
heirynhr <148991328+heirynhr@users.noreply.github.com>
2026-04-01 20:15:40 -0400

    Started taking in the input based on requirements

---
+++ src/opt.cpp
@@ -0,0 +1,61 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+// take input from file to get alphabet values
+// like sequence alignment bc you need the common substring /compairsoins being made mismatches and gaps
+// like rna secondary structure bc we are going to be splitting it into two different intervals
+
+// RETURN MAXIMUM TOTAL VALUE OF THE CHARACTERS WITHIN A SUBSTRING
+
+int main(int argc, char** argv) {
+	// Read in file information
+	if(argc < 2){
+		cout << "Invalid input" << endl;
+		return 1;
+	}
+	string filename = argv[1];
+
+	ifstream in(filename);
+	if(!in){
+		throw runtime_error("Could not open input file");
+	}
+
+	//empty file?
+	if (in.peek() == ifstream::traits_type::eof()) {
+		throw runtime_error("empty file");
+	}
+
+	// first line should be numver of letters
+	int numofAlphabets;
+	in >> numofAlphabets;
+	if (!(in >> numofAlphabets)) {
+		throw runtime_error("Could not read K");
+	}
+	// get all the alphabet values create a dictonary/map wher alphabet['a'] = 4
+	unordered_map<string, int> alphabetValues;
+	char currentLetter;
+	int currentValue = 0;
+	for (int i = 0; i < numofAlphabets; ++i) {
+		if (in >> currentLetter >> currentValue ) {
+			alphabetValues[currentLetter] = currentValue;
+		}
+		else {
+			//umm make sure we get them all?
+			throw runtime_error("Didnt read entire alphabet");
+		}
+	}
+
+	// get strings
+	string stringA;
+	string stringB;
+	in >> stringA;
+	in >> stringB;
+
+	//OPT: go through every number that is not the length so while we are not at the max lenght of A or B THEN....
+}
